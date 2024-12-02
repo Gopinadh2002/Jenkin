a29dc9ef6d27025c48a0579edf12954cd150f3db
Gopinadh polukonda <156587993+Gopinadh2002@users.noreply.github.com>
2024-12-02 21:50:15 +0530

    Delete jenkin.java

--- jenkin.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("");  // Closing quotation mark added
-        System.out.println("Hello from GITHub");  // No changes here, it was fine
-        System.out.println("");  // Closing quotation mark added
-    }
-}
