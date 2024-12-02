a754cbe2d3e9c3f71947c3ed2c33d8edd207e9e7
Gopinadh polukonda <156587993+Gopinadh2002@users.noreply.github.com>
2024-12-02 21:11:07 +0530

    Update main.c
    changed code

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-void main()
-{
-int a = 6, b = 10,c;
-c=a+b;
-printf("%d",c);
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("");  // Closing quotation mark added
+        System.out.println("Hello from GITHub");  // No changes here, it was fine
+        System.out.println("");  // Closing quotation mark added
+    }
 }
